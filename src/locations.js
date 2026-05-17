3cf8fd6d4e0a4293f7121210b819797fd5ea30fd
unknown <marianopepe234@gmail.com>
2026-05-17 14:19:45 +0200

    Añadido sistema de juego interactivo con Mapillary y Leaflet

--- src/locations.js
+++ src/locations.js
@@ -1,32 +1,32 @@
 export const LOCATIONS = [
   {
     name: "La Alhambra, Granada",
-    lat: 37.176078, 
-    lng: -3.587584,
-    mapillaryId: "504286121408107" // ID real
+    lat: 37.177070030743764, 
+    lng: -3.5897240552010317,
+    mapillaryId: "485897002718608" // ID real
   },
   {
-    name: "Plaza de España, Sevilla",
-    lat: 37.377241,
-    lng: -5.986942,
-    mapillaryId: "244199854580226" // ID real
+    name: "La Alhambra, Granada",
+    lat: 37.177070030743764, 
+    lng: -3.5897240552010317,
+    mapillaryId: "485897002718608" // ID real
   },
   {
-    name: "Puente Nuevo, Ronda",
-    lat: 36.740829,
-    lng: -5.166013,
-    mapillaryId: "2160155204187063" // ID real
+    name: "La Alhambra, Granada",
+    lat: 37.177070030743764, 
+    lng: -3.5897240552010317,
+    mapillaryId: "485897002718608" // ID real
   },
   {
-    name: "Mezquita de Córdoba",
-    lat: 37.879193,
-    lng: -4.779383,
-    mapillaryId: "669919507664654" // ID real
+    name: "La Alhambra, Granada",
+    lat: 37.177070030743764, 
+    lng: -3.5897240552010317,
+    mapillaryId: "485897002718608" // ID real
   },
   {
-    name: "Torre del Oro, Sevilla",
-    lat: 37.383033,
-    lng: -5.990225,
-    mapillaryId: "776735583561917" // ID real
+    name: "La Alhambra, Granada",
+    lat: 37.177070030743764, 
+    lng: -3.5897240552010317,
+    mapillaryId: "485897002718608" // ID real
   }
 ];
